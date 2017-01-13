d7c80ae081572fc263b34ed88db26d3eebd333a9
cmjt <cmjt@st-andrews.ac.uk>
2017-01-13 14:28:03 +1300

    plots

--- R/plots.r
+++ R/plots.r
@@ -13,10 +13,10 @@ plot.smoke <- function(x = NULL, id = NULL){
     if(!is.null(id)){
         id <- id
         x <- subset(x, x$factor == id)
-        }
+    }
     locs <- cbind(x[,'Longitude'],x[,'Latitude'])
-     x <- SpatialPointsDataFrame(locs
-           ,data.frame(type = x[,'factor']))
+    x <- SpatialPointsDataFrame(locs
+                               ,data.frame(type = x[,'factor']))
     if(length(table(x$type)) == 2){ # if there are two factors plot a legend and corresponding coloured dots
         pal <- colorFactor(c( "red","navy"), domain = c("0", "1"))
         map <- leaflet(x) %>% addTiles() %>%
@@ -25,12 +25,12 @@ plot.smoke <- function(x = NULL, id = NULL){
                              radius = ~ifelse(type=="0", 3, 3),
                              stroke = TRUE, fillOpacity = 0.5) %>%
             addLegend(pal = pal, values = na.omit(x$type), opacity = 1,title="Factor",na.label="")
-        }else{
-            map <- leaflet(x) %>% addTiles() %>%
-                setView(mean(locs[,1]),mean(locs[,2]), zoom = 4) %>%
-                addCircleMarkers(data = x,stroke = FALSE, fillOpacity = 0.5,popup = as.character(x$type),
-                                 clusterOptions = markerClusterOptions())
-            }
+    }else{
+        map <- leaflet(x) %>% addTiles() %>%
+            setView(mean(locs[,1]),mean(locs[,2]), zoom = 4) %>%
+            addCircleMarkers(data = x,stroke = FALSE, fillOpacity = 0.5,popup = as.character(x$type),
+                             clusterOptions = markerClusterOptions())
+    }
     map
 }
         
