de09f2c5ab136dcb9704f6dc1578d20e2768ed27
cmjt <cmjt@st-andrews.ac.uk>
2017-01-16 14:13:39 +1300

    add trends

--- R/sim_smoke.r
+++ R/sim_smoke.r
@@ -2,19 +2,19 @@
 #' *************time and space are independent ATM********************
 #' @return a 2xn matrix of values with logging locations, hour of day (1:23), day of week (1:7), satisfaction scores (1:20)
 #' and craving scores (1:20)
-#' *****************8need to add in parameters for tod, wday, etc. trends********************
+#' *****************need to add in parameters for tod, wday, etc. trends********************
 #' @param x  either 1) a matrix of locations, 2) a spatial density object as \code{class} \code{im},
 #' or 3) a model fit object as returned by \code{fit_smoke} (yet to add in).
 #' 
 #'
 #' @export
 
-sim.smoke <- function(x = NULL){
+sim.smoke <- function(x = NULL, sp = NULL, tod.prob = rep(1/23, times = 23), dow.prob = rep(1/7,times = 7)){
     if(class(x)=="im"){
         im <- x
     }else{
         if(is.matrix(x)){
-            w <- ripras(x)
+            if(!is.null(sp)){w <- as.owin(sp)}else{w <- ripras(x)}
             x <- suppressWarnings(ppp(x[,1],x[,2],window = w))
             im <- density(x)
         }else{
@@ -22,12 +22,11 @@ sim.smoke <- function(x = NULL){
         }
     }
     out <- rpoispp(im)
-    tod <- sample(1:23, out$n,replace = TRUE)
-    dow <- sample(1:7, out$n,replace = TRUE)
+    tod <- sample(1:23, out$n,replace = TRUE, prob = tod.prob)
+    dow <- sample(1:7, out$n,replace = TRUE, prob = dow.prob)
     c <- sample(1:20, out$n,replace = TRUE)
-    s <- sample(1:20, out$n,replace = TRUE)
-    out <- cbind(out$x, out$y,tod,dow,c,s)
-    colnames(out) <- c("x","y","Hour","Day","craving","satisfaction")
+    out <- cbind(out$x, out$y,tod,dow,c)
+    colnames(out) <- c("x","y","Hour","Day","craving")
     out
 }
     
