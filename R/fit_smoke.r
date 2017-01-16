870e24c5542adc6204b9174931374490833a403a
cmjt <cmjt@st-andrews.ac.uk>
2017-01-17 12:13:00 +1300

    adding a spatio-temporal model fit

--- R/fit_smoke.r
+++ R/fit_smoke.r
@@ -7,53 +7,29 @@
 #' @param mesh delauney triangulation of area, an object returned by \link{make.mesh} is suitable.
 #' @param locs a matrix of \code{nrow} locations in \code{ncol} dimesions.
 #' @param mark a vector of length \code{nrow} of marks refering to each point location must be zeros and ones
+#' @param t a numeric vector specifying a temporal index for each observation (starting at 1.....T).
 #' @param verbose Logical, if \code{TRUE}, model fitting is output
 #' the console.
-#'
+#' @param hyper prior given to the joint interaction parameter, for a marked model only. By default
+#' this is Normal(0,10)
+#' @param prior.rho prior for the temporal correlation coefficient, by default a \code{pcprior} is used
+#' with \code{param=c(0-0.9)}.
+#' @param control.inla a list as supplied by call to \code{inla} see http://www.r-inla.org/
 #' @importMethodsFrom Matrix diag
 #' 
 #' @export
-fit.smoke <- function(mesh = NULL, locs=NULL,  mark = NULL, verbose = FALSE,
-                      hyper = list(theta=list(prior='normal', param = c(0,10)))){
-    spde <- inla.spde2.matern(mesh, alpha=2)
-    ##create sub point patterns from the mark 
-    locs.s <- subset(locs,mark == 1)
-    locs.c <-subset(locs,mark == 0)
-   
-    Ast.s <- inla.spde.make.A(mesh = mesh, loc = locs.s)
-    Ast.c <- inla.spde.make.A(mesh = mesh, loc = locs.c)
+fit.smoke <- function(mesh = NULL, locs=NULL,  mark = NULL, t = NULL, verbose = FALSE,
+                      hyper = list(theta=list(prior='normal', param = c(0,10))),
+                      prior.rho = list(theta = list(prior='pccor1', param = c(0, 0.9))),
+                      control.inla=list(strategy='gaussian',int.strategy = 'eb')){
     
-    ##number of spatial mesh verticies
-    m <- spde$n.spde                 
-    n.s <- nrow(locs.s)                                  
-    n.c <- nrow(locs.c)
-    ## response for the LGCP model and ``effect'' size for poisson likelihood
-                                       
-    y1 <- rep(0:1, c( m, n.s))
-    expected.s <-c(diag(spde$param.inla$M0), rep(0,n.s))
-    y2 <- rep(0:1, c( m, n.c))
-    expected.c <- c(diag(spde$param.inla$M0), rep(0,n.c))
-                                       
-    stk0 <- inla.stack(data=list(y = cbind(y1,NA),expect = expected.s),
-                       A = list(rBind(Diagonal(n = m), Ast.s)),tag="smoke",
-                       effects=list(field1 = 1:m))
-
-    stk1 <- inla.stack(data=list(y=cbind(NA,y2), expect=expected.c),
-                       A=list(rBind(Diagonal(n = m), Ast.c),
-                              rBind(Diagonal(n = m), Ast.c)),tag = "crave",
-                       effects=list(b_field1 = 1:m,field2 = 1:m))
-
-    ##cojoin stacks
-    stack = inla.stack(stk0,stk1)
-
-    formula = y ~ -1 +
-        f(field1, model = spde) +
-        f(field2, model = spde)+
-        f(b_field1, copy = "field1", fixed = FALSE, hyper = hyper ) 
-
-    result = inla(formula, data=inla.stack.data(stack),
-                  family=c("poisson","poisson"),only.hyperparam = FALSE,
-                  control.predictor=list(A=inla.stack.A(stack),compute=TRUE),
-                  control.compute = list(config = TRUE),
-                  verbose = verbose)
+   if(!is.null(mark)){
+       result <- fit.smoke.spatial.joint(mesh = mesh, locs = locs,  mark = mark, verbose = verbose,
+                                    hyper = hyper, control.inla = control.inla)
+           }
+    if(!is.null(time)){
+        result <- lgcpSPDE:::fit.lgcp(mesh = mesh, locs = locs, temp = t, prior.rho = prior.rho,
+                                      verbose = verbose, control.inla = control.inla)
+        }
+   result
 }
--- R/helpers.r
+++ R/helpers.r
@@ -51,3 +51,56 @@ extract <- function(x = NULL, locs = NULL,mesh = NULL, t = NULL, sd = FALSE){
         
     
    
+## fit spatial only  marked model
+
+fit.smoke.spatial.joint <- function(mesh = NULL, locs=NULL,  mark = NULL, verbose = FALSE,
+                                    hyper = list(theta=list(prior='normal', param = c(0,10))),
+                                    control.inla=list(strategy='gaussian',int.strategy = 'eb')){
+    n.marks <- length(table(mark))
+    ## Error if there are more than two marks
+    if (n.marks != 2){
+        stop("Number of marks should be 2")
+    }
+    spde <- inla.spde2.matern(mesh, alpha=2)
+    ##create sub point patterns from the mark 
+    locs.s <- subset(locs,mark == 1)
+    locs.c <-subset(locs,mark == 0)
+   
+    Ast.s <- inla.spde.make.A(mesh = mesh, loc = locs.s)
+    Ast.c <- inla.spde.make.A(mesh = mesh, loc = locs.c)
+    
+    ##number of spatial mesh verticies
+    m <- spde$n.spde                 
+    n.s <- nrow(locs.s)                                  
+    n.c <- nrow(locs.c)
+    ## response for the LGCP model and ``effect'' size for poisson likelihood
+                                       
+    y1 <- rep(0:1, c( m, n.s))
+    expected.s <-c(diag(spde$param.inla$M0), rep(0,n.s))
+    y2 <- rep(0:1, c( m, n.c))
+    expected.c <- c(diag(spde$param.inla$M0), rep(0,n.c))
+                                       
+    stk0 <- inla.stack(data=list(y = cbind(y1,NA),expect = expected.s),
+                       A = list(rBind(Diagonal(n = m), Ast.s)),tag="smoke",
+                       effects=list(field1 = 1:m))
+
+    stk1 <- inla.stack(data=list(y=cbind(NA,y2), expect=expected.c),
+                       A=list(rBind(Diagonal(n = m), Ast.c),
+                              rBind(Diagonal(n = m), Ast.c)),tag = "crave",
+                       effects=list(b_field1 = 1:m,field2 = 1:m))
+
+    ##cojoin stacks
+    stack = inla.stack(stk0,stk1)
+
+    formula = y ~ -1 +
+        f(field1, model = spde) +
+        f(field2, model = spde)+
+        f(b_field1, copy = "field1", fixed = FALSE, hyper = hyper ) 
+
+    result = inla(formula, data=inla.stack.data(stack),
+                  family=c("poisson","poisson"),only.hyperparam = FALSE,
+                  control.predictor=list(A=inla.stack.A(stack),compute=TRUE),
+                  control.compute = list(config = TRUE),
+                  control.inla = control.inla,
+                  verbose = verbose)
+}
