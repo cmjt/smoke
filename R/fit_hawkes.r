ef5f0da699893ec7b0e04843e1a3bcb2d3444341
cmjt <cmjt@st-andrews.ac.uk>
2017-02-14 16:04:57 +1300

    add method optim

--- R/fit_hawkes.r
+++ R/fit_hawkes.r
@@ -4,17 +4,18 @@
 #'
 #' @param params a numeric vector of length three of starting values
 #' for the paramerers mu, alpha, beta respectively.
-#' @param times a numeric vector of times  
+#' @param times a numeric vector of times
+#' @param method the method used by optim for maximisation by default "L-BFGS-B"
 #' @return estimates parameter values
 #' 
 #' @export
 
-fit.hawkes <- function(params, times, marks = NULL){
+fit.hawkes <- function(params, times, marks = NULL, method = "L-BFGS-B"){
     if(!is.null(marks)){
-        fit <- optim(params,loglik.marked.hawkes, times = times, marks = marks)
+        fit <- optim(params,loglik.marked.hawkes, times = times, marks = marks,method = method)
         rownames <- c("mu","alpha","beta","nu","delta")
     }else{
-        fit<- optim(params,loglik.hawkes, times = times)
+        fit<- optim(params,loglik.hawkes, times = times,method = method)
         rownames <- c("mu","alpha","beta")
     }
     pars <- fit$par
