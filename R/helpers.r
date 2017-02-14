73ba7958b81bc421500a27ff0c41aacd553f109d
cmjt <cmjt@st-andrews.ac.uk>
2017-02-14 18:09:44 +1300

    add Haversine distance

--- R/helpers.r
+++ R/helpers.r
@@ -197,3 +197,19 @@ loglik.marked.hawkes <- function(params, times,marks){
     term_3 <- sum(log(mu_i + alpha_i*Ai*delta_i*exp(-delta_i*marks.t)))
     return(-term_1- term_2 -term_3)
 }
+
+## Haversine
+hdist <- function (lon1, lat1, lon2, lat2){
+    rad <- pi/180
+    a1 <- lat1 * rad
+    a2 <- lon1 * rad
+    b1 <- lat2 * rad
+    b2 <- lon2 * rad
+    dlon <- b2 - a2
+    dlat <- b1 - a1
+    a <- (sin(dlat/2))^2 + cos(a1) * cos(b1) * (sin(dlon/2))^2
+    c <- 2 * atan2(sqrt(a), sqrt(1 - a))
+    R <- 6378.145
+    d <- R * c
+    return(d)
+}
